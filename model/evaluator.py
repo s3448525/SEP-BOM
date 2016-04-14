a1b07bc207cd603bf0dd672b60c3bf3b5c4d880c
Jared-D <jared42d@gmail.com>
2016-04-14 18:55:14 +1000

    Added stub evaluator function.

---
+++ model/evaluator.py
@@ -0,0 +1,12 @@
+
+
+def evaluate(location, time=datetime.utcnow(), weather_type='rain', obs_source='wow', forecast_source='BOM'):
+    # Find the forecast value closest to 'location' and 'time' of type
+    # 'weather_type' from 'forecast_source'.
+    #TODO
+    # Find the observation value closest to 'location' and 'time' of type
+    # 'weather_type' from 'observation_source'.
+    #TODO
+    # Compare the forecast value and observation value.
+    #TODO
+    # Return the result.
